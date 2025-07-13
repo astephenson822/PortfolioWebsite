ed049811fbee4c04c1151b5a1f0e885f40d044a5
Andrew-M-Stephenson <amstephenson3@crimson.ua.edu>
2025-07-12 20:50:39 -0500

    changed resume section

--- portfolio_website/src/components/pages/Resume.js
+++ portfolio_website/src/components/pages/Resume.js
@@ -4,18 +4,12 @@ import '../../App.css';
 export default function Resume() {
   return (
     <div className="resume-container">
-      <div className="download-link">
-        Download Resume{' '}
-        <a href={`${process.env.PUBLIC_URL}/Resume.pdf`} download>
-          here
-        </a>
-      </div>
       <div className="pdf-viewer">
         <iframe
           src={`${process.env.PUBLIC_URL}/Resume.pdf`}
           title="Resume"
           width="100%"
-          height="800px"
+          height="1000px"
         />
       </div>
     </div>
